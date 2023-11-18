5fd869a2a47db2526b328b9a3e3b7972b54c0c7b
Dylan A. Pitts <pitts.dylan@gmail.com>
2023-11-18 14:04:00 -0500

    Update nvim-tree

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -1,47 +1,7 @@
+local map = vim.keymap.set
+
+map('n', '|', ':NvimTreeToggle<CR>')
+
 require("nvim-tree").setup()
 
-local api = require("nvim-tree.api")
 
-local function edit_or_open()
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    -- expand or collapse folder
-    api.node.open.edit()
-  else
-    -- open file
-    api.node.open.edit()
-    -- Close the tree if file was opened
-    api.tree.close()
-  end
-end
-
--- open as vsplit on current node
-local function vsplit_preview()
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    -- expand or collapse folder
-    api.node.open.edit()
-  else
-    -- open file as vsplit
-    api.node.open.vertical()
-  end
-
-  -- Finally refocus on tree if it was lost
-  api.tree.focus()
-end
-
-on_attach = funtion(bufnr)
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
--- global
-vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
-
--- on_attach
-vim.keymap.set("n", "l", edit_or_open,          opts("Edit Or Open"))
-vim.keymap.set("n", "L", vsplit_preview,        opts("Vsplit Preview"))
-vim.keymap.set("n", "h", api.tree.close,        opts("Close"))
-vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
